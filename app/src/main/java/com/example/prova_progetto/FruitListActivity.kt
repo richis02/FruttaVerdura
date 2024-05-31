9e65214fbfbe9bfc237d3eedf9fbd279b4a25472
richis02 <riccardo.scalco.1@studenti.unipd.it>
2024-05-31 17:56:48 +0200

    modifiche a FruitListActivity ma sebra che l'intent non passi l'id in modo corretto (o la query di insert non funziona)

--- app/src/main/java/com/example/prova_progetto/FruitListActivity.kt
+++ app/src/main/java/com/example/prova_progetto/FruitListActivity.kt
@@ -1,13 +1,19 @@
 package com.example.prova_progetto
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class FruitListActivity : ComponentActivity(){
 
     override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_fruit_list)
+        setContentView(R.layout.activity_fruit_list)
+
+        val string = intent.getStringExtra( "list_key")
+
+        val tv: TextView = findViewById(R.id.tv)
+        tv.text = "Lista id: $string"
 
     }
 }
