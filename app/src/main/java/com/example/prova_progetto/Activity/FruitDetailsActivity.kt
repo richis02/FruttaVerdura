f4c35b7eb0cd0c33dbb246f75fc10c199333b2da
gerepao <geredrums@gmail.com>
2024-06-02 12:08:05 +0200

    elim comment

--- app/src/main/java/com/example/prova_progetto/Activity/FruitDetailsActivity.kt
+++ app/src/main/java/com/example/prova_progetto/Activity/FruitDetailsActivity.kt
@@ -8,13 +8,13 @@ import com.example.prova_progetto.db.FruitVegViewModel
 import com.example.prova_progetto.db.FruitVegViewModelFactory
 
 class FruitDetailsActivity: ComponentActivity() {
-    //TODO: RICORDARSI DI TOGLIERLA IN FASE DI MERGE
-    //      -> CLASSE FANTOCCIO
 
     private val fruitVegViewModel: FruitVegViewModel by viewModels {
         FruitVegViewModelFactory((application as FruitVegApplication).repository)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
     }
 }
